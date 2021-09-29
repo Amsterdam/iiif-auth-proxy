7ef6cd22f97db5d6899b9a28d40aee9478587c66
Hielke <kramerh@gmail.com>
2021-09-29 12:26:48 +0200

    Move consume_raw code into a try/except

--- src/iiif/ingress_zip_consumer.py
+++ src/iiif/ingress_zip_consumer.py
@@ -27,32 +27,37 @@ class ZipConsumer(BaseConsumer):
     set_consume_started_at = True
 
     def consume_raw_data(self, raw_data):
-        record = json.loads(raw_data)
+        try:
+            record = json.loads(raw_data)
 
-        # Download all the files from the source systems through cantaloupe
-        tmp_folder_path, info_txt_contents, zipjob_uuid = cantaloupe.download_files_for_zip(record)
+            # Download all the files from the source systems through cantaloupe
+            tmp_folder_path, info_txt_contents, zipjob_uuid = cantaloupe.download_files_for_zip(record)
 
-        # Store the info_file_along_with_the_image_files
-        zip_tools.save_file_to_folder(tmp_folder_path, 'report.txt', info_txt_contents)
+            # Store the info_file_along_with_the_image_files
+            zip_tools.save_file_to_folder(tmp_folder_path, 'report.txt', info_txt_contents)
 
-        # Zip all files together
-        zip_file_path = zip_tools.create_local_zip_file(zipjob_uuid, tmp_folder_path)
-        zip_file_name = os.path.basename(zip_file_path)
+            # Zip all files together
+            zip_file_path = zip_tools.create_local_zip_file(zipjob_uuid, tmp_folder_path)
+            zip_file_name = os.path.basename(zip_file_path)
 
-        # Move the file to the object store
-        conn = object_store.get_object_store_connection()
-        object_store.store_object_on_object_store(conn, zip_file_path, zip_file_name)
+            # Move the file to the object store
+            conn = object_store.get_object_store_connection()
+            object_store.store_object_on_object_store(conn, zip_file_path, zip_file_name)
 
-        # Create a temporary url
-        temp_zip_download_url = object_store.create_object_store_temp_url(
-            conn,
-            zip_file_name,
-            expiry_days=settings.TEMP_URL_EXPIRY_DAYS)
+            # Create a temporary url
+            temp_zip_download_url = object_store.create_object_store_temp_url(
+                conn,
+                zip_file_name,
+                expiry_days=settings.TEMP_URL_EXPIRY_DAYS)
 
-        # Send the email
-        email_subject = "Downloadlink Bouw- en omgevingdossiers"
-        email_body = render_to_string('download_zip.html', {'temp_zip_download_url': temp_zip_download_url})
-        mailing.send_email(record['email_address'], email_subject, email_body)
+            # Send the email
+            email_subject = "Downloadlink Bouw- en omgevingdossiers"
+            email_body = render_to_string('download_zip.html', {'temp_zip_download_url': temp_zip_download_url})
+            mailing.send_email(record['email_address'], email_subject, email_body)
 
-        # Cleanup the local zip file and folder with images
-        zip_tools.cleanup_local_files(zip_file_path, tmp_folder_path)
+            # Cleanup the local zip file and folder with images
+            zip_tools.cleanup_local_files(zip_file_path, tmp_folder_path)
+
+        except Exception as e:
+            log.error("ingress_zip_consumer_error:", e)
+            raise e
