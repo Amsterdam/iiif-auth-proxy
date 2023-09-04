fbcf485214e2a2655d6b283ef0df4ff8bb69b930
Hielke Kramer <kramerh@gmail.com>
2023-09-04 23:41:14 +0200

    Fixed correct resizing

--- src/iiif/image_formatting.py
+++ src/iiif/image_formatting.py
@@ -1,6 +1,8 @@
 import logging
+from io import BytesIO
 
 from django.http import HttpResponse
+from PIL import Image
 
 from iiif import tools
 
@@ -28,38 +30,50 @@ def parse_scaling_string(scaling):
 
     return desired_width, desired_height
 
-def scale_image(content, scaling, format):
-    """
-    Resize an image from given content.
 
-    :param content: The content of the image (usually response.content from requests)
-    :param scaling: The scaling string from the url (either 'full' or '100,50' in which 
-                    100=max width and 50=max height)
-    :return: Scaled image
+def scale_image(content, scaling, content_type):
+    """
+    Scale the image to the desired size. Never scale up.
+
+    :param content: The image data
+    :param scaling: The scaling string from the url
+    :param content_type: The content type of the image
+    :return: The scaled image data
     """
     if scaling.lower() == "full":
         return content
 
-    # TODO: The cod below is WIP
-    # desired_width, desired_height = parse_scaling_string(scaling)
-    #
-    # img = Image.open(BytesIO(content))
-    #
-    # aspect_ratio = img.width / img.height
-    # if aspect_ratio > 1:
-    #     new_width = desired_width
-    #     new_height = int(desired_width / aspect_ratio)
-    # elif aspect_ratio < 1:
-    #     new_width = int(desired_height * aspect_ratio)
-    #     new_height = desired_height
-    # else:
-    #     new_width = desired_width
-    #     new_height = desired_height
-    #
-    # scaled_image = img.resize((new_width, new_height), Image.LANCZOS)
-    #
-    # image_stream = BytesIO()
-    # scaled_image.save(image_stream, format=format)
-    # scaled_image_data = image_stream.getvalue()
-    #
-    # return scaled_image_data
+    desired_width, desired_height = parse_scaling_string(scaling)
+
+    img = Image.open(BytesIO(content))
+
+    aspect_ratio = img.width / img.height
+    if desired_width is None:
+        new_width = int(desired_height * aspect_ratio)
+        new_height = desired_height
+    elif desired_height is None:
+        new_width = desired_width
+        new_height = int(desired_width / aspect_ratio)
+    else:
+        width_reduction_percentage = (img.width - desired_width) / img.width
+        height_reduction_percentage = (img.height - desired_height) / img.height
+
+        if width_reduction_percentage >= height_reduction_percentage:
+            new_width = desired_width
+            new_height = int(desired_width / aspect_ratio)
+        else:
+            new_width = int(desired_height * aspect_ratio)
+            new_height = desired_height
+
+    # Ensure we don't scale up
+    if new_width > img.width or new_height > img.height:
+        return content
+
+    scaled_image = img.resize((new_width, new_height), Image.LANCZOS)
+
+    image_stream = BytesIO()
+    format = content_type.split("/")[1].upper()
+    scaled_image.save(image_stream, format=format)
+    scaled_image_data = image_stream.getvalue()
+
+    return scaled_image_data
