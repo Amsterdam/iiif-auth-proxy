a12e32b4d0faf06cc658617cb92d1cb021bee633
Terry van Walen <t.van.walen@amsterdam.nl>
2024-05-07 13:55:32 +0200

    Concat path safely for static image

--- src/main/settings.py
+++ src/main/settings.py
@@ -237,7 +237,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_IMAGE = f"{STATIC_URL}example.jpg"
+STATIC_IMAGE = os.path.join(STATIC_URL, "example.jpg")
 
 
 # Django Logging settings
