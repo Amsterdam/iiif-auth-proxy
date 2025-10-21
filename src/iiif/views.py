cba8a72e8761d07784a8f8aaed4e9c0038899874
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2025-10-21 14:05:37 +0200

    change log exception

--- src/iiif/views.py
+++ src/iiif/views.py
@@ -88,5 +88,5 @@ def index(request, iiif_url):
 
         return add_caching_headers(is_cacheable, HttpResponse(edited_image, file_type))
     except utils.ImmediateHttpResponse as e:
-        log.exception("ImmediateHttpResponse in index:")
+        log.exception(f"ImmediateHttpResponse in index:{e.response.content}")
         return e.response
