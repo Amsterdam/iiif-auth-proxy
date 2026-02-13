f17826fb5d3fba63fec52d3abdf0bf0a93679bb5
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2026-02-13 12:49:34 +0100

    improvement info_txt_contents zip-message

--- tests/test_zip_consumer/test_request_restricted_aanvraag.py
+++ tests/test_zip_consumer/test_request_restricted_aanvraag.py
@@ -145,7 +145,7 @@ def test_request_restricted_aanvraag_in_zip(
 
     assert len(lines) == 3
     assert lines[0].strip() == "The following files were requested:"
-    assert lines[1].strip() == f"test.jpg: Not included in this zip because {RESPONSE_CONTENT_AANVRAAG_RESTRICTED}"
+    assert lines[1].strip() == f"test.jpg: excluded, {RESPONSE_CONTENT_AANVRAAG_RESTRICTED}"
     assert lines[2].strip() == "test-public.jpg: included"
 
     extracted_files = list((tmp_path / f"{fixed_uuid}").iterdir())
